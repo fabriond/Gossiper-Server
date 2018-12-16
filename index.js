35bb88f60121ce9e78e91d0685d9bf3faa423a39
fabriond <gfbf@ic.ufal.br>
2018-12-16 00:46:50 -0300

    Added comments for CORS and changed the routesPath to be the main page

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const commentsController = require("./controllers/comments_controller");
 
 const app = express();
 const commentsPath = "/:codigo/comments"
-const routesPath = "/routes";
+const routesPath = "/";
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+/**
+ * This is used to allow Cross-Origin requests so the frontend and 
+ * backend servers can be run from the same host without problems.
+ * 
+ * Add other frontend host urls to the allowedOrigins array to 
+ * allow Cross-Origin for different hosts
+ */
 app.use((req, res, next) => {
     const allowedOrigins = ['http://localhost:8080', 'https://gossiper-frontend.herokuapp.com', 'http://gossiper-frontend.herokuapp.com'];
     const origin = req.headers.origin;
@@ -25,6 +32,7 @@ app.use((req, res, next) => {
     next();
 });
 
+//init only happens after the database connection is successful
 exports.init = function(){
     app.listen(port, () => {
         console.log("running on port: "+port);
@@ -33,6 +41,7 @@ exports.init = function(){
 
 database.connect();
 
+//main page with routes info
 app.get(routesPath, (req, res) => {
     var output = [];
     app._router.stack.forEach(element => {
